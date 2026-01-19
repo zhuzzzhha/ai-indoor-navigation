958e72d88381fa221c85069914262b3819a09593
zhuzzzhha <allazhuk16@yandex.ru>
2026-01-19 11:54:00 +0300

    run sensor logger

--- source/ronin_resnet.py
+++ source/ronin_resnet.py
@@ -250,7 +250,7 @@ def recon_traj_with_preds(dataset, preds, seq_id=0, **kwargs):
     Reconstruct trajectory with predicted global velocities.
     """
     ts = dataset.ts[seq_id]
-    ind = np.array([i[1] for i in dataset.index_map if i[0] == seq_id], dtype=np.int)
+    ind = np.array([i[1] for i in dataset.index_map if i[0] == seq_id], dtype=np.int32)
     dts = np.mean(ts[ind[1:]] - ts[ind[:-1]])
     pos = np.zeros([preds.shape[0] + 2, 2])
     pos[0] = dataset.gt_pos[seq_id][0, :2]
@@ -304,7 +304,7 @@ def test_sequence(args):
     for data in test_data_list:
         seq_dataset = get_dataset(root_dir, [data], args, mode='test')
         seq_loader = DataLoader(seq_dataset, batch_size=1024, shuffle=False)
-        ind = np.array([i[1] for i in seq_dataset.index_map if i[0] == 0], dtype=np.int)
+        ind = np.array([i[1] for i in seq_dataset.index_map if i[0] == 0], dtype=np.int32)
 
         targets, preds = run_test(network, seq_loader, device, True)
         losses = np.mean((targets - preds) ** 2, axis=0)
---
+++ source/run_from_sensorlogger.py
@@ -0,0 +1,112 @@
+
+import pandas as pd
+import numpy as np
+import os
+import glob
+import argparse
+
+def process(path, model_path):
+    create_ronin_pickle(path)
+    run_ronin(path, model_path)
+    create_pdr_csv(path)
+
+def create_ronin_pickle(path):
+  print(f"Creating RoNIN format pickle from SensorLogger data in {path}" )
+  acc = pd.read_csv(path + "/TotalAcceleration.csv")
+  gyr = pd.read_csv(path + "/Gyroscope.csv")
+  rv = pd.read_csv(path + "/Orientation.csv") 
+
+  df = pd.DataFrame()
+  
+  # Sample and interpolate data to 200 Hz
+  ts = np.arange(acc.seconds_elapsed.iloc[0], acc.seconds_elapsed.iloc[-1], 0.005)
+
+  df["seconds_elapsed"] = ts
+  
+  df["time"] = ts * 1e9 # to nanoseconds for RoNIN
+
+  df["acce_x"] = np.interp(ts, acc.seconds_elapsed, acc.x)
+  df["acce_y"] = np.interp(ts, acc.seconds_elapsed, acc.y)
+  df["acce_z"] = np.interp(ts, acc.seconds_elapsed, acc.z)
+
+  df["gyro_x"] = np.interp(ts, gyr.seconds_elapsed, gyr.x)
+  df["gyro_y"] = np.interp(ts, gyr.seconds_elapsed, gyr.y)
+  df["gyro_z"] = np.interp(ts, gyr.seconds_elapsed, gyr.z)
+
+  df["game_rv_w"] = np.interp(ts, rv.seconds_elapsed, rv.qw)
+  df["game_rv_x"] = np.interp(ts, rv.seconds_elapsed, rv.qx)
+  df["game_rv_y"] = np.interp(ts, rv.seconds_elapsed, rv.qy)
+  df["game_rv_z"] = np.interp(ts, rv.seconds_elapsed, rv.qz)
+
+  filename = path + "/processed/data.pkl"
+  os.makedirs(os.path.dirname(filename), exist_ok=True)
+  print(f"Saving RoNIN format pickle (size: {len(df)}) to {filename}")
+  df.to_pickle(filename)
+
+def run_ronin(path, model_path):
+    print("Running RoNIN...")
+    cmd = f"python source/ronin_resnet.py" \
+      f" --mode test --dataset ridi --test_path {path}" \
+      f" --out_dir {path}/processed --model_path {model_path}"
+    os.system(cmd)
+
+def _annotate_pdr(pdr_df, path):
+    annotation_path = os.path.join(path, "Annotation.csv")
+    if not os.path.exists(annotation_path):
+        return
+    try:
+      annotations = pd.read_csv(annotation_path)
+    except:
+      # Fails for some other reason (like empty file)
+      return
+    print(f"Found {len(annotations)} annotations, adding to PDR CSV")
+
+    pdr_df["annotation"] = ""
+    
+    for s, text in zip(annotations.seconds_elapsed, annotations.text):
+        gt_ix = (pdr_df.seconds_elapsed - s).abs().idxmin()
+        pdr_df.loc[gt_ix, "annotation"] = text
+
+
+def create_pdr_csv(path):
+    print("Creating PDR CSV")
+    xy_file = glob.glob(path + "/processed/*.npy", recursive=True)[0]
+    xy_data = np.load(xy_file)
+    x = xy_data[:, 0]
+    y = xy_data[:, 1]
+
+    df_ronin = pd.read_pickle(path + "/processed/data.pkl")
+    df = pd.DataFrame()
+    df["seconds_elapsed"] = df_ronin.seconds_elapsed
+    df["t"] = df_ronin.seconds_elapsed # Just for convenience
+
+    if len(x) != len(df):
+        print(f"Warning: Mismatch in lengths: npy={len(x)} vs pkl={len(df)}")
+        print("Truncating to the shorter length (should not be a big problem)")
+        min_len = min(len(x), len(df))
+        x = x[:min_len]
+        y = y[:min_len]
+        df = df[:min_len]
+
+    df["x"] = x
+    df["y"] = y
+
+    _annotate_pdr(df, path)
+
+    pdr_path = path + "/processed/pdr.csv"
+    print(f"Saving PDR output to {pdr_path}")
+    df.to_csv(pdr_path, index=False)
+
+def main():
+  parser=argparse.ArgumentParser()
+
+  parser.add_argument("--path", help="Data path for the Sensorlogger data")
+  parser.add_argument("--model_path", help="RoNIN model path")
+  
+  args=parser.parse_args()
+
+  process(args.path, args.model_path)
+
+
+if __name__ == "__main__":
+    main()
